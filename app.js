7c9b629c8dd829286b2d4712ca80128a830500b3
sabrinachs <sabrinach08@yahoo.com.br>
2022-08-03 22:31:05 -0300

    Início do header

--- app.js
+++ app.js
@@ -10,9 +10,54 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => {
+app.get("/adicao_receita", (req, res) => {
+  res.render("adicao_receita", {
+    title: "Adicionar receita",
+    links: [
+      { href: "/index", label: "Home"},
+      { href: "/receitas", label: "Receitas"},
+      { href: "/destaques", label: "Destaques"}
+    ]
+  });
+});
+
+app.get("/destaques", (req, res) => {
+  res.render("destaques", {
+    title: "Destaques",
+    links: [
+      { href: "/index", label: "Home"},
+      { href: "/receitas", label: "Receitas"},
+      { href: "/destaques", label: "Destaques"}
+    ]
+  });
+});
+
+app.get("/edicao_receita", (req, res) => {
+  res.render("edicao_receita", {
+    title: "Editar receita",
+    links: [
+      { href: "/index", label: "Home"},
+      { href: "/receitas", label: "Receitas"},
+      { href: "/destaques", label: "Destaques"}
+    ]
+  });
+});
+
+
+app.get("/index", (req, res) => {
   res.render("index", {
     title: "Página inicial",
+    links: [
+      { href: "/index", label: "Home"},
+      { href: "/receitas", label: "Receitas"},
+      { href: "/destaques", label: "Destaques"}
+    ]
+  });
+});
+
+app.get("/receitas-individual", (req, res) => {
+  res.render("receitas-individual", {
+    title: "Receita",
     links: [
       { href: "/index", label: "Home"},
       { href: "/receitas", label: "Receitas"},
@@ -32,16 +77,6 @@ app.get("/receitas", (req, res) => {
   });
 });
 
-app.get("/destaques", (req, res) => {
-  res.render("destaques", {
-    title: "Destaques",
-    links: [
-      { href: "/index", label: "Home"},
-      { href: "/receitas", label: "Receitas"},
-      { href: "/destaques", label: "Destaques"}
-    ]
-  });
-});
 
 app.listen(port, () => {
   console.log(`Server running at ${port}`)
