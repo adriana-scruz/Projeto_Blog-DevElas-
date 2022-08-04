1989d8a4b6421116bfde8bda759df604e1c18a14
sabrinachs <sabrinach08@yahoo.com.br>
2022-08-03 21:38:11 -0300

    Header

--- app.js
+++ app.js
@@ -10,9 +10,38 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Página inicial",
+    links: [
+      { href: "/index", label: "Home"},
+      { href: "/receitas", label: "Receitas"},
+      { href: "/destaques", label: "Destaques"}
+    ]
+  });
+});
 
+app.get("/receitas", (req, res) => {
+  res.render("receitas", {
+    title: "Receitas",
+    links: [
+      { href: "/index", label: "Home"},
+      { href: "/receitas", label: "Receitas"},
+      { href: "/destaques", label: "Destaques"}
+    ]
+  });
+});
 
-
+app.get("/destaques", (req, res) => {
+  res.render("destaques", {
+    title: "Destaques",
+    links: [
+      { href: "/index", label: "Home"},
+      { href: "/receitas", label: "Receitas"},
+      { href: "/destaques", label: "Destaques"}
+    ]
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server running at ${port}`)
