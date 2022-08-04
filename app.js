5456a51eb2229f56d8eda36ebfb1a8624e211eae
sabrinachs <sabrinach08@yahoo.com.br>
2022-08-03 21:24:32 -0300

    Estrutura do front-end

--- app.js
+++ app.js
@@ -1,33 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const bodyParser= require('body-parser')
 const port = 3000;
 
 const fs = require('fs')
 
-function novaReceita(receita){
-    let rawdata = fs.readFileSync('./receitas.json');
-    //parsing o arquivo JSON em um objeto JS
-    let colecaoReceitas = JSON.parse(rawdata);
-    colecaoReceitas.push(receita)
-    let data = JSON.stringify(colecaoReceitas);
-    fs.writeFileSync('./receitas.json', data);
-    return colecaoReceitas
-}
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req,res) =>{
-    res.sendFile(__dirname + '/index.html')
+
+
+
+app.listen(port, () => {
+  console.log(`Server running at ${port}`)
 })
-
-app.post('/novoPost', (req, res) => {
-    console.log('Post funciona!');
-    const colecaoReceitas = novaReceita(req.body);
-    res.send(colecaoReceitas)
-    //res.send(req.body)
-  })
-
-app.listen(port, function() {
-    console.log('listening on 3000')
-  })
